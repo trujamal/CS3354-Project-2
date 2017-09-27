c61cd796398e2c818c7e8dd4dc31f1735b549880
Zach <zs1046@txstate.edu>
2017-09-26 20:06:24 -0500

    Classes

--- src/Envelope.java
+++ src/Envelope.java
@@ -1,2 +1,22 @@
 public class Envelope extends ShippingStore {
+    /***************************************************************************
+     * VARIABLE DEFINITIONS
+     **************************************************************************/
+
+    // Define private member variables.
+    private int height;      // Holds Envelopes Height in inches
+    private int width;      // Holds Envelopes Width in inches
+
+
+    /***************************************************************************
+     * CONSTRUCTORS
+     **************************************************************************/
+    /*
+      Default Constructor (Initialize to "0" or "").
+    */
+    public Envelope(){
+        super();
+        height = 0;
+        width = "";
+    }
 }
