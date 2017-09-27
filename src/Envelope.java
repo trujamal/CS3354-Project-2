2327ac4f815c4194ea23ec013a8a33df7fd424ac
Zach <zs1046@txstate.edu>
2017-09-26 20:20:54 -0500

    Classes

--- src/Envelope.java
+++ src/Envelope.java
@@ -1,4 +1,6 @@
-public class Envelope extends ShippingStore {
+import java.io.Serializable;
+
+public class Envelope extends ShippingStore implements Serializable {
     /***************************************************************************
      * VARIABLE DEFINITIONS
      **************************************************************************/
@@ -17,6 +19,22 @@ public class Envelope extends ShippingStore {
     public Envelope(){
         super();
         height = 0;
-        width = "";
+        width = 0;
     }
+    /*
+      Overloaded Constructor (Initialize to user specified values)
+      @param trackingnumber the item's unique trackingnumber.
+      @param type the item's available type (>= 0).
+      @param specification the item's price (> 0.00).
+      @param mailingclass the item's title.
+      @param isbn the item's unique ISBN.
+      @param author the item's specific author.
+    */
+    public Envelope(String trackingnumber, String type, String specification, String mailingclass
+                float weight, int volume, ){
+        super(trackingnumber, type, specification, mailingclass, weight, volume);
+        this.height = height;
+        this.width = width;
+    }
+
 }
